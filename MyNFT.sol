1dfa59ae8393c605f1a6eba0f39a208de6e4490c
lon <107249654+lon123456789@users.noreply.github.com>
2025-08-14 22:14:24 +0200

    Update MyNFT.sol
    add NFT contract

--- MyNFT.sol
+++ MyNFT.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFT is ERC721 {
+    uint256 public nextTokenId;
+    address public admin;
+
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+        admin = msg.sender;
+    }
+
+    function mint(address to) external {
+        require(msg.sender == admin, "only admin");
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
+    }
+}
